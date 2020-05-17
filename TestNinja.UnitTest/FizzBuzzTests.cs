dc202921eafe7ed8512558b4afa5f4ae1356aaf4
Khuthadzo Tshikotshi <khuthadzotshikotshi@gmail.com>
2020-05-17 18:03:52 +0200

    name test cases

--- TestNinja.UnitTest/FizzBuzzTests.cs
+++ TestNinja.UnitTest/FizzBuzzTests.cs
@@ -6,10 +6,10 @@ namespace TestNinja.UnitTest
     [TestFixture]
     public class FizzBuzzTests
     {
-        [TestCase(15, "FizzBuzz")]
-        [TestCase(10, "Buzz")]
-        [TestCase(9, "Fizz")]
-        [TestCase(4, "4")]
+        [TestCase(15, "FizzBuzz", TestName = "Number divisible by 3 and 5 (Returns FizzBuzz)")]
+        [TestCase(10, "Buzz", TestName = "Number only divisible by 5 (Returns Buzz)")]
+        [TestCase(9, "Fizz", TestName = "Number only divisible by 3 (Returns Fizz)")]
+        [TestCase(4, "4", TestName = "Number not divisble by 3 or 5 (Returns input number as string)")]
         public void GetOutput_ValidOutput(int input, string output){
             var result = FizzBuzz.GetOutput(input);
 
